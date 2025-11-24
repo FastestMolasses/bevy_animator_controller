15e34128afdbb91cbafdddeca90e395d044478e8
Abe M <abe.malla8@gmail.com>
2025-11-23 17:42:01 -0800

    clean up

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,13 +1,8 @@
-use bevy::{
-    prelude::*,
-    render::{mesh::PrimitiveTopology, render_asset::RenderAssetUsages},
-};
+use bevy::prelude::*;
 use bevy_animator_controller::{OzzAnimationPlugin, prelude::*};
 use bevy_asset_loader::prelude::*;
 use ozz_animation_rs::*;
 use std::sync::Arc;
-
-const MAX_DEBUG_BONE_COUNT: usize = 64;
 
 #[derive(States, Default, Clone, Eq, PartialEq, Debug, Hash)]
 pub enum GameState {
@@ -38,48 +33,6 @@ fn main() {
         .add_systems(OnEnter(GameState::Playing), (setup_scene,))
         // .add_systems(Update, update_debug_bone_transforms)
         .run();
-}
-
-#[derive(Component)]
-pub struct DebugBone;
-
-pub(crate) fn setup_debug_bones(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let bone_mesh = meshes.add(build_bone_mesh());
-    let bone_material = materials.add(Color::srgb(0.68, 0.68, 0.8));
-    for i in 0..MAX_DEBUG_BONE_COUNT {
-        commands.spawn((
-            Mesh3d(bone_mesh.clone()),
-            MeshMaterial3d(bone_material.clone()),
-            Transform::from_xyz(0.0, 0.0, 0.0),
-            BoneIndex(i),
-            DebugBone,
-        ));
-    }
-}
-
-fn update_debug_bone_transforms(
-    mut query: Query<(&mut Transform, &mut Visibility, &BoneIndex), With<DebugBone>>,
-    controller_query: Query<&AnimatorController, Without<BoneIndex>>,
-) {
-    let Ok(controller) = controller_query.single() else {
-        return;
-    };
-
-    let bone_trans: &Vec<OzzTransform> = &controller.bone_trans;
-    if !bone_trans.is_empty() {
-        for (mut transform, mut visibility, idx) in query.iter_mut() {
-            if idx.0 < bone_trans.len() {
-                *visibility = Visibility::Visible;
-                transform.translation = bone_trans[idx.0].position;
-                transform.rotation = bone_trans[idx.0].rotation;
-                transform.scale = bone_trans[idx.0].scale;
-            }
-        }
-    }
 }
 
 fn setup_scene(
@@ -157,44 +110,4 @@ pub(crate) fn build_player_animation_controller(
         vec![animation_layer],
         parameters,
     ))
-}
-
-#[rustfmt::skip]
-fn build_bone_mesh() -> Mesh {
-    let c = [Vec3::new(1.0, 0.0, 0.0),
-        Vec3::new(0.2, 0.1, 0.1),
-        Vec3::new(0.2, 0.1, -0.1),
-        Vec3::new(0.2, -0.1, -0.1),
-        Vec3::new(0.2, -0.1, 0.1),
-        Vec3::new(0.0, 0.0, 0.0)];
-    let n = [Vec3::cross(c[2] - c[1], c[2] - c[0]).normalize(),
-        Vec3::cross(c[1] - c[2], c[1] - c[5]).normalize(),
-        Vec3::cross(c[3] - c[2], c[3] - c[0]).normalize(),
-        Vec3::cross(c[2] - c[3], c[2] - c[5]).normalize(),
-        Vec3::cross(c[4] - c[3], c[4] - c[0]).normalize(),
-        Vec3::cross(c[3] - c[4], c[3] - c[5]).normalize(),
-        Vec3::cross(c[1] - c[4], c[1] - c[0]).normalize(),
-        Vec3::cross(c[4] - c[1], c[4] - c[5]).normalize()];
-
-    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default())
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vec![
-        c[0], c[2], c[1],
-        c[5], c[1], c[2],
-        c[0], c[3], c[2],
-        c[5], c[2], c[3],
-        c[0], c[4], c[3],
-        c[5], c[3], c[4],
-        c[0], c[1], c[4],
-        c[5], c[4], c[1],
-    ])
-    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![
-        n[0], n[0], n[0],
-        n[1], n[1], n[1],
-        n[2], n[2], n[2],
-        n[3], n[3], n[3],
-        n[4], n[4], n[4],
-        n[5], n[5], n[5],
-        n[6], n[6], n[6],
-        n[7], n[7], n[7],
-    ])
 }
